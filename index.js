0ff379af3e30981aa9534af75be3a4aa31724a81
Rasaki Solebo <arosplebo@gmail.com>
2022-02-03 23:05:07 -0600

    add graffiti to the dog site

--- index.js
+++ index.js
@@ -2,3 +2,16 @@
 // Select some elements...
 let header = document.querySelector('#page-header')
 header.style.color = 'blue'
+
+let dogimages = document.querySelectorAll('.dog-image')
+for(let i =0; i < dogimages.length; i++){
+    dogimages[i].style.borderRadius = '50px'
+}
+
+let dogname = document.querySelectorAll('.dog-name')
+for(let i =0; i < dogname.length; i++){
+    dogname[i].style.textAlign = 'left'
+}
+
+let footer = document.querySelector('.footer')
+footer.style.color = 'blue'
